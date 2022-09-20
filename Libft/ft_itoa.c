f3c130aaba87bee4a421ceb8c0a8ccc73d333755
PacoFrost <ranciscoxd@gmail.com>
2022-09-20 22:23:41 +0200

    itoa
    itoa ^w^

--- Libft/ft_itoa.c
+++ Libft/ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: fragarci <fragarci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/20 12:59:14 by fragarci          #+#    #+#             */
-/*   Updated: 2022/09/20 13:01:16 by fragarci         ###   ########.fr       */
+/*   Updated: 2022/09/20 22:21:08 by fragarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,4 +19,48 @@ RETURN VALUE
 	The string representing the number. NULL if the memory reservation fails.
 */
 
-char	*ft_itoa(int n);
+#include "libft.h"
+
+static int	positive(int num)
+{
+	if (num < 0)
+		return (-num);
+	else
+		return (num);
+}
+
+static size_t	get_size(int num)
+{
+	size_t	size;
+
+	size = 0;
+	if (num <= 0)
+		size++;
+	while (num != 0)
+	{
+		size++;
+		num /= 10;
+	}
+	return (size);
+}
+
+char	*ft_itoa(int n)
+{
+	char	*ret;
+	size_t	size;
+
+	size = get_size(n);
+	ret = (char *)malloc((sizeof(char) * size) + 1);
+	ret[size] = '\0';
+	if (n < 0)
+		ret[0] = '-';
+	else if (n == 0)
+		ret[0] == '0';
+	while (n != 0)
+	{
+		size--;
+		ret[size] = positive(n % 10) + '0';
+		n /= 10;
+	}
+	return (ret);
+}
