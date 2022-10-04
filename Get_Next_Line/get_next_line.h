37332654d312917752d12a9a380ddcd8eedb8b11
Francisco Jose Garcia Del Rio <fragarci@c3r10s3.42malaga.com>
2022-10-04 15:04:13 +0200

    get_next_line

--- Get_Next_Line/get_next_line.h
+++ Get_Next_Line/get_next_line.h
@@ -6,16 +6,25 @@
 /*   By: fragarci <fragarci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/26 12:18:47 by fragarci          #+#    #+#             */
-/*   Updated: 2022/09/27 09:49:49 by fragarci         ###   ########.fr       */
+/*   Updated: 2022/10/04 10:43:24 by fragarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# include <unistd.h>
 # include <stdlib.h>
+# include <unistd.h>
 
-# define BUFFER_SIZE = 42
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+size_t	ft_strlen(char *str);
+char	*ft_strchr(char *str, int c);
+char	*ft_strjoin(char *full_str, char *buffer);
+char	*ft_get_line(char *full_str);
+char	*ft_get_remain(char *full_str);
+char	*get_next_line(int fd);
 
 #endif
--- Get_Next_Line/get_next_line_utils.c
+++ Get_Next_Line/get_next_line_utils.c
@@ -6,8 +6,120 @@
 /*   By: fragarci <fragarci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/26 12:15:22 by fragarci          #+#    #+#             */
-/*   Updated: 2022/09/27 11:57:51 by fragarci         ###   ########.fr       */
+/*   Updated: 2022/10/04 10:42:54 by fragarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+size_t	ft_strlen(char *str)
+{
+	size_t	i;
+
+	i = 0;
+	if (!str)
+		return (0);
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+char	*ft_strchr(char *str, int c)
+{
+	int	i;
+
+	i = 0;
+	if (!str)
+		return (0);
+	if (c == '\0')
+		return ((char *)&str[ft_strlen(str)]);
+	while (str[i] != '\0')
+	{
+		if (str[i] == (char) c)
+			return ((char *)&str[i]);
+		i++;
+	}
+	return (0);
+}
+
+char	*ft_strjoin(char *full_str, char *buff)
+{
+	size_t	i;
+	size_t	j;
+	char	*str;
+
+	if (!full_str)
+	{
+		full_str = (char *)malloc(1 * sizeof(char));
+		full_str[0] = '\0';
+	}
+	if (!full_str || !buff)
+		return ((void *)0);
+	str = malloc(sizeof(char) * ((ft_strlen(full_str) + ft_strlen(buff)) + 1));
+	if (str == (void *)0)
+		return ((void *)0);
+	i = -1;
+	j = 0;
+	if (full_str)
+		while (full_str[++i] != '\0')
+			str[i] = full_str[i];
+	while (buff[j] != '\0')
+		str[i++] = buff[j++];
+	str[ft_strlen(full_str) + ft_strlen(buff)] = '\0';
+	free(full_str);
+	return (str);
+}
+
+char	*ft_get_line(char *full_str)
+{
+	int		i;
+	char	*str;
+
+	i = 0;
+	if (!full_str[i])
+		return ((void *)0);
+	while (full_str[i] && full_str[i] != '\n')
+		i++;
+	str = (char *)malloc(sizeof(char) * (i + 2));
+	if (!str)
+		return ((void *)0);
+	i = 0;
+	while (full_str[i] && full_str[i] != '\n')
+	{
+		str[i] = full_str[i];
+		i++;
+	}
+	if (full_str[i] == '\n')
+	{
+		str[i] = full_str[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+
+char	*ft_get_remain(char *full_str)
+{
+	int		i;
+	int		j;
+	char	*str;
+
+	i = 0;
+	while (full_str[i] && full_str[i] != '\n')
+		i++;
+	if (!full_str[i])
+	{
+		free(full_str);
+		return ((void *)0);
+	}
+	str = (char *)malloc(sizeof(char) * (ft_strlen(full_str) - i + 1));
+	if (!str)
+		return ((void *)0);
+	i++;
+	j = 0;
+	while (full_str[i])
+		str[j++] = full_str[i++];
+	str[j] = '\0';
+	free(full_str);
+	return (str);
+}
