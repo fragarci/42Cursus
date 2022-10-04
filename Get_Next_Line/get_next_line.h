3fd8fccf89b70ce19d16d62a14147e094156ea4b
PacoFrost <ranciscoxd@gmail.com>
2022-10-04 16:33:00 +0200

    test

--- Get_Next_Line/get_next_line.h
+++ Get_Next_Line/get_next_line.h
@@ -6,14 +6,13 @@
 /*   By: fragarci <fragarci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/26 12:18:47 by fragarci          #+#    #+#             */
-/*   Updated: 2022/09/27 09:49:49 by fragarci         ###   ########.fr       */
+/*   Updated: 2022/10/04 00:28:29 by fragarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# include <unistd.h>
 # include <stdlib.h>
 
 # define BUFFER_SIZE = 42
--- Get_Next_Line/get_next_line_utils.c
+++ Get_Next_Line/get_next_line_utils.c
@@ -6,8 +6,63 @@
 /*   By: fragarci <fragarci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/26 12:15:22 by fragarci          #+#    #+#             */
-/*   Updated: 2022/09/27 11:57:51 by fragarci         ###   ########.fr       */
+/*   Updated: 2022/10/04 01:14:07 by fragarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+size_t ft_strlen(char *str)
+{
+	size_t	i;
+
+	i = 0;
+	if (!str)
+		return (0);
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	char	*str;
+
+	str = (char *)s;
+	while (*str != (char)c)
+	{
+		if (*str == '\0')
+			return (NULL);
+		str++;
+	}
+	return (str);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	int		i;
+	size_t	len1;
+	size_t	len2;
+	char	*ret;
+
+	if (!s1 || !s2)
+		return ((void *)0);
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+	ret = (char *)malloc(sizeof(char) * (len1 + len2) + 1);
+	if (!ret)
+		return ((void *)0);
+	i = -1;
+	while (++i < (int)len1)
+		ret[i] = s1[i];
+	i = -1;
+	while (++i < (int)len2)
+		ret[len1 + i] = s2[i];
+	ret[len1 + i] = '\0';
+	return (ret);
+}
+
+char	*ft_get_line(char *full_str)
+{
+	
+}
