ec4d68dad98ab0badd2fbcb3fe9fb7ae71c386db
Francisco Jose Garcia Del Rio <fragarci@c3r2s3.42malaga.com>
2023-08-08 14:55:38 +0200

    Cagoendio con los triangulos òwó

--- CPP/cpp02/ex03/bsp.cpp
+++ CPP/cpp02/ex03/bsp.cpp
@@ -3,30 +3,55 @@
 /*                                                        :::      ::::::::   */
 /*   bsp.cpp                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fragarci <fragarci@student.42malaga.com    +#+  +:+       +#+        */
+/*   By: fragarci <fragarci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/07 11:20:41 by fragarci          #+#    #+#             */
-/*   Updated: 2023/08/07 12:19:17 by fragarci         ###   ########.fr       */
+/*   Updated: 2023/08/08 14:51:01 by fragarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Point.hpp"
 
-// Se toma un triangulo con vertices a, b y c y un punto point.
-// realizamos el producto vectorial entre los vectores formados por los puntos
-// del triangulo y el punto "point" y comparamos su signo.
-// Si los signos de los productos vectoriales son iguales para todos los lados
-// del triangulo, el punto "point" se encuentra dentro de este.
-
 bool bsp(Point const a, Point const b, Point const c, Point const point)
 {
-    bool check[3];
+	bool check[3];
 
-    check[0] = ((a.get_X() - point.get_X()) * (b.get_Y() - a.get_Y()) -
-                (b.get_X() - a.get_X()) * (point.get_Y() - a.get_Y())) > 0;
-    check[1] = ((b.get_X() - point.get_X()) * (c.get_Y() - b.get_Y()) -
-                (c.get_X() - b.get_X()) * (point.get_Y() - b.get_Y())) > 0;
-    check[2] = ((c.get_X() - point.get_X()) * (a.get_Y() - c.get_Y()) -
-                (a.get_X() - c.get_X()) * (point.get_Y() - c.get_Y())) > 0;
-    return (check[0] == check[1] && check[1] == check[2]);
+
+	std::cout << std::boolalpha
+
+		<< "a = " << a << std::endl
+		<< "b = " << b << std::endl
+		<< "c = " << c << std::endl << std::endl;
+
+	check[0] = (Fixed(-1) * (b.get_Y() - a.get_Y()) * (a.get_X() - point.get_X())) + ((b.get_X() - a.get_X()) * (a.get_Y() - point.get_Y())) > 0;
+	check[1] = (Fixed(-1) * (c.get_Y() - b.get_Y()) * (b.get_X() - point.get_X())) + ((c.get_X() - b.get_X()) * (b.get_Y() - point.get_Y())) > 0;
+	check[2] = (Fixed(-1) * (a.get_Y() - c.get_Y()) * (c.get_X() - point.get_X())) + ((a.get_X() - c.get_X()) * (c.get_Y() - point.get_Y())) > 0;
+
+	/*std::cout << std::boolalpha
+	<< "\ncheck[0] = " << Fixed((Fixed(-1) * (b.get_Y() - a.get_Y()) * (a.get_X() - point.get_X())) + ((b.get_X() - a.get_X()) * (a.get_Y() - point.get_Y())))
+	<< "\ncheck[1] = " << Fixed((Fixed(-1) * (c.get_Y() - b.get_Y()) * (b.get_X() - point.get_X())) + ((c.get_X() - b.get_X()) * (b.get_Y() - point.get_Y())))
+	<< "\ncheck[2] = " << Fixed((Fixed(-1) * (a.get_Y() - c.get_Y()) * (c.get_X() - point.get_X())) + ((a.get_X() - c.get_X()) * (c.get_Y() - point.get_Y())))
+	<< std::endl;*/
+
+	return (check[0] == check[1] && check[1] == check[2]);
 }
+/*
+	|--> Points A, B and vector AB represents one side of a triangle <--|
+	A(Ax, Ay)
+	B(Bx, By)
+	AB(Bx - Ax, By - Ay)
+
+	|--> We need to find if point P is inside the triangle <--|
+	P(Px, Py)
+
+	|--> PA is the vector that goes from P to A (The start point of the vector AB) <--|
+	PA(Ax - Px, Ay - Py)
+
+	|--> P_AB is perpendicular to AB <--|
+	P_AB(-(By - Ay), Bx - Ax)
+
+	|--> Now we make the dot product between P_AB and PA <--|
+	-(By - Ay) · (Ax - Px) + (Bx - Ax) · (Ay - Py)
+
+	|--> If the dot product is grater than zero, P is at the right side otherwise it's at the left. If its zero is at the same line <--|
+*/
--- CPP/cpp02/ex03/main.cpp
+++ CPP/cpp02/ex03/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fragarci <fragarci@student.42malaga.com    +#+  +:+       +#+        */
+/*   By: fragarci <fragarci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/25 13:24:21 by fragarci          #+#    #+#             */
-/*   Updated: 2023/08/07 12:16:18 by fragarci         ###   ########.fr       */
+/*   Updated: 2023/08/08 14:50:44 by fragarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,12 @@ int	main(void)
 	Point const b = Point(0, 5);
 	Point const c = Point(5, 0);
 	Point const p = Point(-1, -1);
-	std::cout << std::boolalpha
 
+	/*std::cout
 		<< "a = " << a << std::endl
 		<< "b = " << b << std::endl
 		<< "c = " << c << std::endl
 		<< "p = " << p << std::endl
-		<< "Point inside triangle: " << bsp(a, b, c, p) << std::endl;
+		<< "Point inside triangle: " << std::endl;*/
+		std::cout << std::boolalpha << bsp(a, b, c, p) << std::endl;
 }
