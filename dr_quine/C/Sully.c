32ee40af733ba323da1bcc14ba5821cbcbd65c4b
Francisco Jose Garcia Del Rio <fragarci@c2r7s5.42malaga.com>
2025-05-12 10:07:51 +0200

    shrink

--- dr_quine/C/Sully.c
+++ dr_quine/C/Sully.c
@@ -1,29 +1,5 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-#define CODE "#include <stdio.h>%1$c#include <stdlib.h>%1$c#include <string.h>%1$c%1$c#define CODE %2$c%3$s%2$c%1$c%1$cint main(void)%1$c{%1$c%5$cint i = %4$d;%1$c%5$cif (i <= 0) return (0);%1$c%5$cchar current_file[100];%1$c%5$csprintf(current_file, %2$cSully%%d.c%2$c, i);%1$c%5$cif (!strcmp(current_file, __FILE__))i--;%1$c%5$cchar new_file[100];%1$c%5$csprintf(new_file, %2$cSully%%d.c%2$c, i);%1$c%5$cchar new_exec[100];%1$c%5$csprintf(new_exec, %2$cSully%%d%2$c, i);%1$c%5$cFILE *f = fopen(new_file, %2$cw%2$c);%1$c%5$cif (f == NULL) return (1);%1$c%5$cfprintf(f, CODE, 10, 34, CODE, i, 9);%1$c%5$cfclose(f);%1$c%5$cchar compile[300];%1$c%5$csprintf(compile, %2$cgcc -o %%s %%s%2$c, new_exec, new_file);%1$c%5$csystem(compile);%1$c%5$cchar run_cmd[200];%1$c%5$csprintf(run_cmd, %2$c./%%s%2$c, new_exec);%1$c%5$csystem(run_cmd);%1$c%5$creturn (0);%1$c}%1$c"
-
-int main(void)
-{
-    int i = 5;
-    if (i <= 0) return (0);
-    char current_file[10];
-    sprintf(current_file, "Sully_%d.c", i);
-    if (!strcmp(current_file, __FILE__))i--;
-    char new_file[10];
-    sprintf(new_file, "Sully_%d.c", i);
-    char new_exec[10];
-    sprintf(new_exec, "Sully_%d", i);
-    FILE *f = fopen(new_file, "w");
-    if (f == NULL) return (1);
-    fprintf(f, CODE, 10, 34, CODE, i, 9);
-    fclose(f);
-    char compile[100];
-    sprintf(compile, "gcc -o %s %s", new_exec, new_file);
-    system(compile);
-    char run_cmd[100];
-    sprintf(run_cmd, "./%s", new_exec);
-    system(run_cmd);
-    return (0);
-}
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#define CODE "#include<stdio.h>%1$c#include<stdlib.h>%1$c#include<string.h>%1$c#define CODE %2$c%3$s%2$c%1$cint main(void){int i=%4$d;if(i<=0)return 0;char c[100];sprintf(c,%2$cSully_%%d.c%2$c,i);if(!strcmp(c,__FILE__))i--;char n[100];sprintf(n,%2$cSully_%%d.c%2$c,i);char e[100];sprintf(e,%2$cSully_%%d%2$c,i);FILE*f=fopen(n,%2$cw%2$c);if(f==NULL)return 1;fprintf(f,CODE,10,34,CODE,i);fclose(f);char o[300];sprintf(o,%2$cgcc -o %%s %%s%2$c,e,n);system(o);char r[200];sprintf(r,%2$c./%%s%2$c,e);system(r);return 0;}%1$c"
+int main(void){int i=5;if(i<=0)return 0;char c[10];sprintf(c,"Sully_%d.c",i);if(!strcmp(c,__FILE__))i--;char n[10];sprintf(n,"Sully_%d.c",i);char e[10];sprintf(e,"Sully_%d",i);FILE*f=fopen(n,"w");if(f==NULL)return 1;fprintf(f,CODE,10,34,CODE,i);fclose(f);char o[100];sprintf(o, "gcc -o %s %s", e, n);system(o);char r[100];sprintf(r,"./%s",e);system(r);return 0;}
